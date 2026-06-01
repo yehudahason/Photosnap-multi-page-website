8a1646b0c39ae9836f8be76f5be94c81375a3324
Yehuda Hason <yehudaxt@gmail.com>
2026-06-01 13:06:36 +0300

    first commit

---
+++ src/components/Header.tsx
@@ -0,0 +1,23 @@
+export default function Header() {
+  return (
+    <header>
+      <h1>Header</h1>
+      <nav>
+        <ul>
+          <li>
+            <a href="/">Home</a>{" "}
+          </li>
+          <li>
+            <a href="/stories">Stories</a>{" "}
+          </li>
+          <li>
+            <a href="/features">Features</a>{" "}
+          </li>
+          <li>
+            <a href="/pricing">Pricing</a>{" "}
+          </li>
+        </ul>
+      </nav>
+    </header>
+  );
+}
---
+++ src/router.tsx
@@ -0,0 +1,50 @@
+import { createBrowserRouter } from "react-router-dom";
+import Header from "./components/Header.tsx";
+import Home from "./pages/Home.tsx";
+import Footer from "./components/Footer.tsx";
+import Stories from "./pages/Stories.tsx";
+import Pricing from "./pages/Pricing.tsx";
+import Features from "./pages/Features.tsx";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: (
+      <>
+        <Header />
+        <Home />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/stories",
+    element: (
+      <>
+        <Header />
+        <Stories />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "/pricing",
+    element: (
+      <>
+        <Header />
+        <Pricing />
+        <Footer />
+      </>
+    ),
+  },
+  {
+    path: "features",
+    element: (
+      <>
+        <Header />
+        <Features />
+        <Footer />
+      </>
+    ),
+  },
+]);
