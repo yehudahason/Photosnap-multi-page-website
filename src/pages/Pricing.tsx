968d0c4238cba1d2b46a983d99d7e754ec00dda7
Yehuda Hason <yehudaxt@gmail.com>
2026-06-08 15:38:06 +0300

    page pricing

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -4,10 +4,20 @@ export default function Pricing() {
   const baseUrl = import.meta.env.BASE_URL;
   return (
     <main>
-      <section className="img-container-3">
-        <div className="grid-3">
-          <div className="img-item">
-            <img src={`${baseUrl}/assets/pricing/desktop/basic.jpg`} alt="" />
+      <section className="img-container-4">
+        <div className="grid-4">
+          <div className="item">
+            <div className="text">
+              <h1> Pricing</h1>
+              <p>
+                Create a your stories, Photosnap is a platform for photographers
+                and visual storytellers. It’s the simple way to create and share
+                your photos.
+              </p>
+            </div>
+          </div>
+          <div className="item-img p">
+            <img src={`${baseUrl}/assets/pricing/desktop/hero.jpg`} alt="" />
           </div>
         </div>
       </section>
