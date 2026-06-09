523d45e8133291f3aaf5efc294e6e57865551cbc
Yehuda Hason <yehudaxt@gmail.com>
2026-06-09 13:58:21 +0300

    checkmark-btn

--- src/pages/Pricing.tsx
+++ src/pages/Pricing.tsx
@@ -1,7 +1,10 @@
-import React from "react";
+import { useEffect, useState } from "react";
 
 export default function Pricing() {
+  type Plan = "monthly" | "yearly";
   const baseUrl = import.meta.env.BASE_URL;
+  const [plan, setPlan] = useState<boolean>(false);
+
   return (
     <main>
       <section className="img-container-4">
@@ -21,6 +24,57 @@ export default function Pricing() {
           </div>
         </div>
       </section>
+      <section className="plan-container">
+        <div className="plan-btn">
+          <span className={`monthly ${plan ? "" : "active"}`}>Monthly</span>
+          <div className="checkbox-container">
+            <input
+              type="checkbox"
+              checked={plan}
+              onChange={() => setPlan((prev) => !prev)}
+            />
+            <span className="checkmark"></span>
+          </div>
+          <span className={`monthly ${plan ? "active" : ""}`}>Yearly</span>
+        </div>
+      </section>
+      <table>
+        <tr>
+          <th>Company</th>
+          <th>Contact</th>
+          <th>Country</th>
+        </tr>
+        <tr>
+          <td>Alfreds Futterkiste</td>
+          <td>Maria Anders</td>
+          <td>Germany</td>
+        </tr>
+        <tr>
+          <td>Centro comercial Moctezuma</td>
+          <td>Francisco Chang</td>
+          <td>Mexico</td>
+        </tr>
+        <tr>
+          <td>Ernst Handel</td>
+          <td>Roland Mendel</td>
+          <td>Austria</td>
+        </tr>
+        <tr>
+          <td>Island Trading</td>
+          <td>Helen Bennett</td>
+          <td>UK</td>
+        </tr>
+        <tr>
+          <td>Laughing Bacchus Winecellars</td>
+          <td>Yoshi Tannamuri</td>
+          <td>Canada</td>
+        </tr>
+        <tr>
+          <td>Magazzini Alimentari Riuniti</td>
+          <td>Giovanni Rovelli</td>
+          <td>Italy</td>
+        </tr>
+      </table>
     </main>
   );
 }
